b9931938c1c36482783c98ccd0595e65b8730e27
Jaramillo0220 <joseangel0220@outlook.es>
2023-03-23 18:06:55 -0600

    cambios

---
+++ U4-Programacion.cpp/Tres-mayusculas-valido.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int sum=0,x;
+char frase[30],resp;
+int main (int argc, char* argv[])
+{
+    do{
+    sum=0;
+    cout<<"\n\nIntroduce la frase: \n";cin>>frase;
+        for (x=0;x<30;x++){
+            switch(frase[x]){case 'A':sum++;break;case 'E':sum++;break;case 'I':sum++;break;
+            case 'O':sum++;break;case 'U':sum++;break;default:break;}}
+    if (sum>=3){cout<<"\nBienvenido!!";}else{cout<<"\nIngreso incorrecto";}
+    cout<<"\n\nQuieres poner alguna otra palabra? (y/n)";cin>>resp;
+    }while(resp=='Y' || resp=='y');
+    return 0;   
+}
